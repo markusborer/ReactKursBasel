a99c05a7387a1993f40bd798d95f499428ce975a
Markus Borer <markus.borer@adesso.ch>
2018-12-03 15:42:32 +0100

    Uebung 8

--- reactsample/src/PersonSearchPanel.js
+++ reactsample/src/PersonSearchPanel.js
@@ -3,8 +3,12 @@ import PersonSearchResult from './PersonSearchResult';
 import PersonSearchForm from './PersonSearchForm';
 import axios from 'axios';
 import ErrorBoundary from './ErrorBoundary';
+import { fromEvent } from 'rxjs';
+import { debounceTime, switchMap } from 'rxjs/operators';
 
 class PersonSearchPanel extends React.Component {
+
+    eventTarget = new EventTarget();
 
     constructor(props) {
         super(props);
@@ -13,13 +17,26 @@ class PersonSearchPanel extends React.Component {
         }
     }
 
-    onChange = (text) => {
-        axios.get('http://localhost:8080/person?name=' + text)
-            .then(res => {
-                this.setState({
-                    persons: res.data
+    componentDidMount() {
+        fromEvent(this.eventTarget, "OnChange")
+            .pipe(
+                //debounceTime(200),
+                switchMap(event => {
+                    return axios.get('http://localhost:8080/person?name=' + event.detail);
                 })
-            })
+            )
+            .subscribe(response => {
+                console.log(response);
+                this.setState({
+                    persons: response.data
+                })
+            });
+    }
+
+    onChange = (text) => {
+        this.eventTarget.dispatchEvent(
+            new CustomEvent('OnChange', { detail: text })
+        );
     }
 
 
